cb82b208ca136eb638735aee65f54e93b9b20c2f
Mike <yakovlev_miha@yahoo.com>
2018-05-05 11:21:25 +0300

    add login

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,20 @@
 let socket = io();
 
+function scrollToBottom() {
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -18,6 +33,7 @@ socket.on('newMessage', function(message) {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -30,16 +46,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   $('#messages').append(html);
-   
-
-  // let li = jQuery('<li></li>');
-  // let a = jQuery('<a target="_blank">My current location</a>');
-  // let formattedTime = moment(message.createdAt).format('h:mm a');
-  
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
